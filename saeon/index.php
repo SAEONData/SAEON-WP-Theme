2d990403be7da74ba695a2c797226be4581b9745
Taryn <taryn@saeon.ac.za>
2020-05-06 10:33:46 +0200

    Base Files Setup

---
+++ saeon/index.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The main template file.
+ */
+
+ ?>
+
+
+		<main id="main" class="post-wrap" role="main">
+
+		<?php if ( have_posts() ) : ?>
+
+			<?php /* Start the Loop */ ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php
+					the_content();
+				?>
+
+			<?php endwhile; ?>
+
+			<?php the_posts_navigation(); ?>
+
+		<?php else : ?>
+
+			no content
+
+		<?php endif; ?>
+
+		</main>
